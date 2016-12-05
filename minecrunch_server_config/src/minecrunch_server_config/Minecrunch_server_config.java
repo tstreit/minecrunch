036a086d76be5d910627d43327f34ad189afdfaf
Ted Streit <tstreit1@gmail.com>
2016-12-05 13:34:24 -0600

    Initial Server Configuration
    Initial Server Configuration

---
+++ minecrunch_server_config/src/minecrunch_server_config/Minecrunch_server_config.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package minecrunch_server_config;
+
+/**
+ *
+ * @author tstreit
+ */
+public class Minecrunch_server_config {
+
+    public static void main(String[] args) {
+        PackUI pui = new PackUI();
+        pui.setVisible(true);
+    }
+}
