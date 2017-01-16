86f1f7aa0649b2c4b6478222546cd4a389159716
Ted Streit <tstreit1@gmail.com>
2017-01-16 13:32:49 -0600

    updates from code review part 2
    updated minucrunch_updater class to minimize redundant code

--- minecrunch_updater/src/minecrunch_updater/Minecrunch_updater.java
+++ minecrunch_updater/src/minecrunch_updater/Minecrunch_updater.java
@@ -38,127 +38,54 @@ public class Minecrunch_updater {
         // Get system properties
         String os = System.getProperty("os.name");
         String home = System.getProperty("user.home");
+        String dir;
+        String newfile2 = null;
 
         if (os.contains("Windows")) {
-            try {
-                // Get version.txt from server
-                URL url = new URL("http://www.minecrunch.net/download/minecrunch_installer/version.txt");
-                File file = new File(home + "\\.minecrunch\\version.txt");
-                File file2 = new File(home + "\\.minecrunch\\resources\\version.txt");
-
-                FileUtils.copyURLToFile(url, file);
-                boolean isTwoEqual = FileUtils.contentEquals(file, file2);
-
-                if (isTwoEqual) {
-                    System.out.println("Up to date.");
-                    Run();
-                } else {
-                    Object[] options = {"Yes, please",
-                        "No way!"};
-                    Component frame = null;
-                    // Get answer if the user wants to update or not
-                    int n = JOptionPane.showOptionDialog(frame,
-                            "There is an update to Minecrunch Modpack.",
-                            "Would you like to update now?",
-                            JOptionPane.YES_NO_OPTION,
-                            JOptionPane.QUESTION_MESSAGE,
-                            null,
-                            options,
-                            options[0]);
-
-                    if (n == JOptionPane.YES_OPTION) {
-                        // If yes run the Update method
-                        Update();
-                    } else {
-                        // If user chooses no then just run the minecrunch_launcher jar file
-                        Run();
-                    }
-                }
-            } catch (IOException ex) {
-                Logger.getLogger(Minecrunch_updater.class.getName()).log(Level.SEVERE, null, ex);
-            }
+            dir = home + "\\.minecrunch\\";
+            newfile2 = dir + "\\resources\\";
+        } else {
+            dir = home + "/.minecrunch/";
+            newfile2 = dir + "/resources/";
         }
 
-        if (os.contains("Linux")) {
-            try {
-                // Get version.txt from server
-                URL url = new URL("http://www.minecrunch.net/download/minecrunch_installer/version.txt");
-                File file = new File(home + "/.minecrunch/version.txt");
-                File file2 = new File(home + "/.minecrunch/resources/version.txt");
+        try {
+            // Get version.txt from server
+            URL url = new URL("http://www.minecrunch.net/download/minecrunch_installer/version.txt");
+            File file = new File(dir + "version.txt");
+            File file2 = new File(newfile2 + "version.txt");
 
-                FileUtils.copyURLToFile(url, file);
-                boolean isTwoEqual = FileUtils.contentEquals(file, file2);
+            FileUtils.copyURLToFile(url, file);
+            boolean isTwoEqual = FileUtils.contentEquals(file, file2);
 
-                if (isTwoEqual) {
-                    System.out.println("Up to date.");
-                    Run();
+            if (isTwoEqual) {
+                System.out.println("Up to date.");
+                Run();
+            } else {
+                Object[] options = {"Yes, please",
+                    "No way!"};
+                Component frame = null;
+                // Get answer if the user wants to update or not
+                int n = JOptionPane.showOptionDialog(frame,
+                        "There is an update to Minecrunch Modpack.",
+                        "Would you like to update now?",
+                        JOptionPane.YES_NO_OPTION,
+                        JOptionPane.QUESTION_MESSAGE,
+                        null,
+                        options,
+                        options[0]);
+
+                if (n == JOptionPane.YES_OPTION) {
+                    // If yes run the Update method
+                    Update();
                 } else {
-                    Object[] options = {"Yes, please",
-                        "No way!"};
-                    Component frame = null;
-                    // Get answer if the user wants to update or not
-                    int n = JOptionPane.showOptionDialog(frame,
-                            "There is an update to Minecrunch Modpack.",
-                            "Would you like to update now?",
-                            JOptionPane.YES_NO_OPTION,
-                            JOptionPane.QUESTION_MESSAGE,
-                            null,
-                            options,
-                            options[0]);
-
-                    if (n == JOptionPane.YES_OPTION) {
-                        // If yes run the Update method
-                        Update();
-                    } else {
-                        // If user chooses no then just run the minecrunch_launcher jar file
-                        Run();
-                    }
-                }
-            } catch (IOException ex) {
-                Logger.getLogger(Minecrunch_updater.class.getName()).log(Level.SEVERE, null, ex);
-            }
-        }
-
-        if (os.contains("Mac")) {
-            try {
-                // Get version.txt from server
-                URL url = new URL("http://www.minecrunch.net/download/minecrunch_installer/version.txt");
-                File file = new File(home + "/.minecrunch/version.txt");
-                File file2 = new File(home + "/.minecrunch/resources/version.txt");
-
-                FileUtils.copyURLToFile(url, file);
-                boolean isTwoEqual = FileUtils.contentEquals(file, file2);
-
-                if (isTwoEqual) {
-                    System.out.println("Up to date.");
+                    // If user chooses no then just run the minecrunch_launcher jar file
                     Run();
-                } else {
-                    Object[] options = {"Yes, please",
-                        "No way!"};
-                    Component frame = null;
-                    // Get answer if the user wants to update or not
-                    int n = JOptionPane.showOptionDialog(frame,
-                            "There is an update to Minecrunch Modpack.",
-                            "Would you like to update now?",
-                            JOptionPane.YES_NO_OPTION,
-                            JOptionPane.QUESTION_MESSAGE,
-                            null,
-                            options,
-                            options[0]);
-
-                    if (n == JOptionPane.YES_OPTION) {
-                        // If yes run the Update method
-                        Update();
-                    } else {
-                        // If user chooses no then just run the minecrunch_launcher jar file
-                        Run();
-                    }
                 }
-            } catch (IOException ex) {
-                Logger.getLogger(Minecrunch_updater.class.getName()).log(Level.SEVERE, null, ex);
             }
+        } catch (IOException ex) {
+            Logger.getLogger(Minecrunch_updater.class.getName()).log(Level.SEVERE, null, ex);
         }
-
     }
 
     // Run update
@@ -167,90 +94,41 @@ public class Minecrunch_updater {
         // Get system properties
         String os = System.getProperty("os.name");
         String home = System.getProperty("user.home");
+        String dir;
+        String newlib = null;
+
+        if (os.contains("Windows")) {
+            dir = home + "\\.minecrunch\\";
+            newlib = dir + "\\lib\\";
+        } else {
+            dir = home + "/.minecrunch/";
+            newlib = dir + "/lib/";
+        }
 
         // Check for .minecrunch directory if it doesn't exist create main directory, sub-directories, then download libraries and resources 
-        if (os.contains("Windows")) {
+        URL lib1 = new URL("http://www.minecrunch.net/download/minecrunch_installer/lib/commons-io-2.4.jar");
+        URL lib2 = new URL("http://www.minecrunch.net/download/minecrunch_installer/lib/json-simple-1.1.1.jar");
+        URL lib3 = new URL("http://www.minecrunch.net/download/minecrunch_installer/lib/zip4j_1.3.2.jar");
+        URL jar1 = new URL("http://www.minecrunch.net/download/minecrunch_installer/minecrunch_launcher.jar");
 
-            URL lib1 = new URL("http://www.minecrunch.net/download/minecrunch_installer/lib/commons-io-2.4.jar");
-            URL lib2 = new URL("http://www.minecrunch.net/download/minecrunch_installer/lib/json-simple-1.1.1.jar");
-            URL lib3 = new URL("http://www.minecrunch.net/download/minecrunch_installer/lib/zip4j_1.3.2.jar");
-            URL jar1 = new URL("http://www.minecrunch.net/download/minecrunch_installer/minecrunch_launcher.jar");
+        File flib1 = new File(newlib + "commons-io-2.4.jar");
+        File flib2 = new File(newlib + "json-simple-1.1.1.jar");
+        File flib3 = new File(newlib + "zip4j_1.3.2.jar");
+        File fjar1 = new File(dir + "minecrunch_launcher.jar");
 
-            File flib1 = new File(home + "\\.minecrunch\\lib\\commons-io-2.4.jar");
-            File flib2 = new File(home + "\\.minecrunch\\lib\\json-simple-1.1.1.jar");
-            File flib3 = new File(home + "\\.minecrunch\\lib\\zip4j_1.3.2.jar");
-            File fjar1 = new File(home + "\\.minecrunch\\minecrunch_launcher.jar");
+        FileUtils.copyURLToFile(lib1, flib1);
+        FileUtils.copyURLToFile(lib2, flib2);
+        FileUtils.copyURLToFile(lib3, flib3);
+        FileUtils.copyURLToFile(jar1, fjar1);
 
-            FileUtils.copyURLToFile(lib1, flib1);
-            FileUtils.copyURLToFile(lib2, flib2);
-            FileUtils.copyURLToFile(lib3, flib3);
-            FileUtils.copyURLToFile(jar1, fjar1);
+        Component frame = null;
+        JOptionPane.showMessageDialog(frame, "Update complete.");
 
-            Component frame = null;
-            JOptionPane.showMessageDialog(frame, "Update complete.");
-
-            try {
-                Process proc = Runtime.getRuntime().exec("java -jar " + home + "\\.minecrunch\\minecrunch_launcher.jar");
-                System.exit(0);
-            } catch (IOException ex) {
-                Logger.getLogger(Minecrunch_updater.class.getName()).log(Level.SEVERE, null, ex);
-            }
-        }
-
-        if (os.contains("Linux")) {
-
-            URL lib1 = new URL("http://www.minecrunch.net/download/minecrunch_installer/lib/commons-io-2.4.jar");
-            URL lib2 = new URL("http://www.minecrunch.net/download/minecrunch_installer/lib/json-simple-1.1.1.jar");
-            URL lib3 = new URL("http://www.minecrunch.net/download/minecrunch_installer/lib/zip4j_1.3.2.jar");
-            URL jar1 = new URL("http://www.minecrunch.net/download/minecrunch_installer/minecrunch_launcher.jar");
-
-            File flib1 = new File(home + "/.minecrunch/lib/commons-io-2.4.jar");
-            File flib2 = new File(home + "/.minecrunch/lib/json-simple-1.1.1.jar");
-            File flib3 = new File(home + "/.minecrunch/lib/zip4j_1.3.2.jar");
-            File fjar1 = new File(home + "/.minecrunch/minecrunch_launcher.jar");
-
-            FileUtils.copyURLToFile(lib1, flib1);
-            FileUtils.copyURLToFile(lib2, flib2);
-            FileUtils.copyURLToFile(lib3, flib3);
-            FileUtils.copyURLToFile(jar1, fjar1);
-
-            Component frame = null;
-            JOptionPane.showMessageDialog(frame, "Update complete.");
-
-            try {
-                Process proc = Runtime.getRuntime().exec("java -jar " + home + "/.minecrunch/minecrunch_launcher.jar");
-                System.exit(0);
-            } catch (IOException ex) {
-                Logger.getLogger(Minecrunch_updater.class.getName()).log(Level.SEVERE, null, ex);
-            }
-        }
-
-        if (os.contains("Mac")) {
-
-            URL lib1 = new URL("http://www.minecrunch.net/download/minecrunch_installer/lib/commons-io-2.4.jar");
-            URL lib2 = new URL("http://www.minecrunch.net/download/minecrunch_installer/lib/json-simple-1.1.1.jar");
-            URL lib3 = new URL("http://www.minecrunch.net/download/minecrunch_installer/lib/zip4j_1.3.2.jar");
-            URL jar1 = new URL("http://www.minecrunch.net/download/minecrunch_installer/minecrunch_launcher.jar");
-
-            File flib1 = new File(home + "/.minecrunch/lib/commons-io-2.4.jar");
-            File flib2 = new File(home + "/.minecrunch/lib/json-simple-1.1.1.jar");
-            File flib3 = new File(home + "/.minecrunch/lib/zip4j_1.3.2.jar");
-            File fjar1 = new File(home + "/.minecrunch/minecrunch_launcher.jar");
-
-            FileUtils.copyURLToFile(lib1, flib1);
-            FileUtils.copyURLToFile(lib2, flib2);
-            FileUtils.copyURLToFile(lib3, flib3);
-            FileUtils.copyURLToFile(jar1, fjar1);
-
-            Component frame = null;
-            JOptionPane.showMessageDialog(frame, "Update complete.");
-
-            try {
-                Process proc = Runtime.getRuntime().exec("java -jar " + home + "/.minecrunch/minecrunch_launcher.jar");
-                System.exit(0);
-            } catch (IOException ex) {
-                Logger.getLogger(Minecrunch_updater.class.getName()).log(Level.SEVERE, null, ex);
-            }
+        try {
+            Process proc = Runtime.getRuntime().exec("java -jar " + dir + "minecrunch_launcher.jar");
+            System.exit(0);
+        } catch (IOException ex) {
+            Logger.getLogger(Minecrunch_updater.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 
@@ -259,33 +137,20 @@ public class Minecrunch_updater {
         // Get system properties
         String os = System.getProperty("os.name");
         String home = System.getProperty("user.home");
+        String dir;
+
+        if (os.contains("Windows")) {
+            dir = home + "\\.minecrunch\\";
+        } else {
+            dir = home + "/.minecrunch/";
+        }
 
         // If user chooses no then just run the minecrunch_launcher jar file
-        if (os.contains("Windows")) {
             try {
-                Process proc = Runtime.getRuntime().exec("java -jar " + home + "\\.minecrunch\\minecrunch_launcher.jar");
+                Process proc = Runtime.getRuntime().exec("java -jar " + dir + "minecrunch_launcher.jar");
                 System.exit(0);
             } catch (IOException ex) {
                 Logger.getLogger(Minecrunch_updater.class.getName()).log(Level.SEVERE, null, ex);
             }
-        }
-
-        if (os.contains("Linux")) {
-            try {
-                Process proc = Runtime.getRuntime().exec("java -jar " + home + "/.minecrunch/minecrunch_launcher.jar");
-                System.exit(0);
-            } catch (IOException ex) {
-                Logger.getLogger(Minecrunch_updater.class.getName()).log(Level.SEVERE, null, ex);
-            }
-        }
-
-        if (os.contains("Mac")) {
-            try {
-                Process proc = Runtime.getRuntime().exec("java -jar " + home + "/.minecrunch/minecrunch_launcher.jar");
-                System.exit(0);
-            } catch (IOException ex) {
-                Logger.getLogger(Minecrunch_updater.class.getName()).log(Level.SEVERE, null, ex);
-            }
-        }
     }
 }
